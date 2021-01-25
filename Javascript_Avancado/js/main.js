133707fd179e0f42675bdc0867c0a35a1e403c4b
renatoabreu14 <renato.abreu.info@gmail.com>
2021-01-25 17:46:02 -0300

    Preparando para atualizar registros Atualizando dados

--- Javascript_Avancado/js/main.js
+++ Javascript_Avancado/js/main.js
@@ -27,7 +27,7 @@ function setList(list){
         table += '<td>'+formatDescription(list[key].description)+'</td>';
         table += '<td>'+list[key].amount+'</td>';
         table += '<td>'+formatValue(list[key].value)+'</td>';
-        table += '<td>Edit | Delete</td>';
+        table += '<td><button onclick="setUpdate('+key+')" class="btn btn-outline-dark">Edit</button> | <button class="btn btn-outline-danger">Delete</button></td>';
         table += '</tr>';
     }
     table += '</tbody>';
@@ -58,6 +58,34 @@ function addData() {
     setList(list);
 }
 
+function setUpdate(id) {
+    var obj = list[id];
+    document.getElementById('description').value = obj.description;
+    document.getElementById('amount').value = obj.amount;
+    document.getElementById('value').value = obj.value;
+    document.getElementById('btnAdd').style.display = "none";
+    document.getElementById('btnUpdate').style.display = "inline-block";
+    document.getElementById('inputIdUpdate').value = id;
+}
+
+function resetForm() {
+    document.getElementById('description').value = "";
+    document.getElementById('amount').value = "";
+    document.getElementById('value').value = "";
+    document.getElementById('inputIdUpdate').value = "";
+    document.getElementById('btnAdd').style.display = "inline-block";
+    document.getElementById('btnUpdate').style.display = "none";
+}
+
+function updateData() {
+    var id = document.getElementById('inputIdUpdate').value;
+    var description = document.getElementById('description').value;
+    var amount = document.getElementById('amount').value;
+    var value = document.getElementById('value').value;
+    list[id] = {"description":description, "amount":amount, "value":value};
+    resetForm();
+    setList(list);
+}
 setList(list);
 
 //console.log(list);
